042802a89a2f613e3475dbe325ecbc1b9a2ea29a
Victor Grycuk <victor@grycuk.net>
2020-11-19 21:57:43 -0300

    Implement own timer

--- streamdeck-magnifier/streamdeck-magnifier/Magnifier.cs
+++ streamdeck-magnifier/streamdeck-magnifier/Magnifier.cs
@@ -4,6 +4,7 @@ using Magnifier.Helpers;
 using System;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Timers;
 
 namespace Magnifier
 {
@@ -11,7 +12,7 @@ namespace Magnifier
     public class Magnifier : PluginBase
     {
         private readonly MagnifierSettings settings;
-        private bool isRunning;
+        private readonly Timer Timer;
         private bool isFixed;
         private Point mouseLocation;
         private DateTime dateTime;
@@ -21,32 +22,16 @@ namespace Magnifier
             settings = payload.Settings == null || payload.Settings.Count == 0
                 ? MagnifierSettings.CreateDefaultSettings()
                 : payload.Settings.ToObject<MagnifierSettings>();
+            
+            Timer = new Timer(10);
+            Timer.Elapsed += new ElapsedEventHandler(UpdateKey);
+            Timer.Enabled = false;
+            Timer.Start();
         }
 
-        public override void Dispose()
+        private void UpdateKey(object sender, ElapsedEventArgs e)
         {
-            Logger.Instance.LogMessage(TracingLevel.INFO, $"Destructor called");
-        }
-
-        public override void KeyPressed(KeyPayload payload)
-        {
-            isRunning = !isRunning;
-            dateTime = DateTime.Now;
-        }
-
-        public override void KeyReleased(KeyPayload payload)
-        {
-            if ((DateTime.Now - dateTime).TotalSeconds > 2)
-            {
-                mouseLocation = ScreenHelper.GetMouseLocation();
-                isFixed = true;
-                isRunning = true;
-            }
-        }
-
-        public override void OnTick()
-        {
-            if (isRunning)
+            if (Timer.Enabled)
             {
                 var location = isFixed ? mouseLocation : ScreenHelper.GetMouseLocation();
                 var img = ImageHelper.CopyFromScreen(settings.ZoomLevel, location);
@@ -60,6 +45,31 @@ namespace Magnifier
                 Connection.SetImageAsync(img);
             }
         }
+
+        public override void Dispose()
+        {
+            Timer.Stop();
+            Timer.Dispose();
+            Logger.Instance.LogMessage(TracingLevel.INFO, $"Destructor called");
+        }
+
+        public override void KeyPressed(KeyPayload payload)
+        {
+            Timer.Enabled = !Timer.Enabled;
+            dateTime = DateTime.Now;
+        }
+
+        public override void KeyReleased(KeyPayload payload)
+        {
+            if ((DateTime.Now - dateTime).TotalSeconds > 2)
+            {
+                mouseLocation = ScreenHelper.GetMouseLocation();
+                isFixed = !isFixed;
+                Timer.Enabled = true;
+            }
+        }
+
+        public override void OnTick() { }
 
         public override void ReceivedSettings(ReceivedSettingsPayload payload)
         {
